99beb038af47883e1a9886adc749accc7cc8df14
Alex <alex.gabriel@mail.ru>
2020-04-01 22:26:48 +0300

    feat: add keyboard render and slight css tweaks

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+/* eslint-env browser */
+
+const TEXTAREA = document.createElement('textarea');
+TEXTAREA.classList.add('textarea');
+const KEYBOARD = document.createElement('div');
+KEYBOARD.classList.add('keyboard');
+const BODY = document.body;
+
 const keyboardEng = [
   ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
   ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete'],
   ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'],
-  ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'shift'],
+  ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift'],
   ['Control', 'Win', 'Alt', ' ', 'Alt', '←', '↓', '→', 'Control'],
 ];
 
@@ -10,20 +18,43 @@ const keyboardEngShift = [
   ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'],
   ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Delete'],
   ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter'],
-  ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '↑', 'shift'],
+  ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '↑', 'Shift'],
   ['Control', 'Win', 'Alt', ' ', 'Alt', '←', '↓', '→', 'Control'],
 ];
 const keyboardRu = [
   ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
   ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Delete'],
   ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
-  ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'shift'],
+  ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'Shift'],
   ['Control', 'Win', 'Alt', ' ', 'Alt', '←', '↓', '→', 'Control'],
 ];
 const keyboardRuShift = [
   ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace'],
   ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Delete'],
   ['CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter'],
-  ['Shift', 'Я', 'Ч', 'С', 'Ь', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '↑', 'shift'],
+  ['Shift', 'Я', 'Ч', 'С', 'Ь', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '↑', 'Shift'],
   ['Control', 'Win', 'Alt', ' ', 'Alt', '←', '↓', '→', 'Control'],
 ];
+const renderTextarea = () => {
+  BODY.appendChild(TEXTAREA);
+};
+const renderKeyboard = (keyboardType) => {
+  const keyboardTypeFlat = keyboardType.flat();
+  keyboardTypeFlat.forEach((e) => {
+    const key = document.createElement('div');
+    key.classList.add('key');
+    key.innerText = e;
+    if (e === ' ') {
+      key.classList.add('Whitespace');
+    }
+    if (e === 'Backspace' || e === 'CapsLock' || e === 'Enter' || e === 'Shift') {
+      key.classList.add(e);
+    }
+    KEYBOARD.appendChild(key);
+  });
+  BODY.appendChild(KEYBOARD);
+};
+
+renderTextarea();
+
+renderKeyboard(keyboardEng);
